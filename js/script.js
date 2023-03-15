f46efffd954caf28055c4fec98c2e9382fddb14b
WoWdeadlock <chandithamahanama@gmail.com>
2023-03-15 20:29:47 +0530

    Added functionality to play single game with pre-entered choice

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 function getComputerChoice() {
 
     const computerGuess = Math.floor(Math.random() * 3); 
-
     switch (computerGuess) {
         case 0:
             return "Rock";
     
         case 1:
-            return "Scissor";
+            return "Scissors";
 
         case 2:
             return "Paper";
@@ -15,6 +14,29 @@ function getComputerChoice() {
 
 }
 
-for (let i = 0; i < 10; i++) {
-    console.log(getComputerChoice());
+function playGame(playerSelection, computerSelection) {
+
+    let playerSelect = playerSelection.toLowerCase();
+    let computerSelect = computerSelection.toLowerCase();
+
+    if (playerSelect === computerSelect) {
+        return "It is a tie!"
+    } else if (
+        (playerSelect === "rock" && computerSelect === "paper") || 
+        (playerSelect === "paper" && computerSelect === "scissors") ||  
+        (playerSelect === "scissors" && computerSelect === "rock"))  
+        {
+        return "You lose! " + computerSelect.charAt(0).toUpperCase() + computerSelect.slice(1) + " Beats " + playerSelect.charAt(0).toUpperCase() + playerSelect.slice(1) + "!";
+    } else if (
+        (computerSelect === "rock" && playerSelect === "paper") || 
+        (computerSelect === "paper" && playerSelect === "scissors") ||  
+        (computerSelect === "scissors" && playerSelect === "rock"))  
+        {
+        return "You win! " + playerSelect.charAt(0).toUpperCase() + playerSelect.slice(1) + " Beats " + computerSelect.charAt(0).toUpperCase() + computerSelect.slice(1) + "!";
+ }
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log(playGame("Rock", getComputerChoice()))  
+}
+
