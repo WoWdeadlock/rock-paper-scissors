646be50d018fc5f877a718d1f6dca3f37e7452b2
WoWdeadlock <chandithamahanama@gmail.com>
2023-03-27 02:06:33 +0530

    decided layout

--- js/script.js
+++ js/script.js
@@ -41,22 +41,148 @@ function playGame(playerSelection, computerSelection) {
  }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+// UI integration
 
-        let playerChoice = prompt("Enter Rock, Paper or Scissors");
+let choiceContainers = document.querySelectorAll(".valid");
+let confirmButton = document.querySelector("#confirm-button");
+let restartButton = document.querySelector("#restart-button");
 
-        console.log(playGame(playerChoice, getComputerChoice()));
-        console.log(winCount);
-        console.log(loseCount);
+let playerChoiceDisplay = document.querySelector("#player");
+let computerChoiceDisplay = document.querySelector("#compute");
+
+let playerScore = document.querySelector("#player-score");
+let computerScore = document.querySelector("#computer-score");
+
+let mainText = document.querySelector("#main-text");
+
+choiceContainers.forEach(choice => {
+
+    let choiceSelected = 0;
+
+    choice.addEventListener("mouseover", () => {
+
+        choice.querySelector(".choice-img").style.width = "225px";
+
+    })
+
+    choice.addEventListener("mouseout", () => {
+
+        if (!(choice.style.border == "2px solid orange")) {
+
+        choice.querySelector(".choice-img").style.width = "200px";
+
+        }
+
+    })
+
+    choice.addEventListener("click", () => {
+
+        confirmButton.classList.remove("hide");
+
+        for (let i = 0; i < 3; i++) {
+
+            if (choiceContainers[i].style.border == "2px solid orange") {
+
+                choiceContainers[i].style.border = "1px solid black";
+                choiceContainers[i].querySelector(".choice-img").style.width = "200px";
+
+            }
+            
+        }
+
+        choice.style.border = "2px solid orange";
+        choice.querySelector(".choice-img").style.width = "225px";
+
+
+    })
+
+});
+
+confirmButton.addEventListener("click", () => {
+
+    let selection = "";
+
+    for (let i = 0; i < 3; i++) {
+
+        if (choiceContainers[i].style.border == "2px solid orange") {
+
+            selection = choiceContainers[i].id;
+            choiceContainers[i].style.border = "1px solid black";
+            choiceContainers[i].querySelector(".choice-img").style.width = "200px";
+
+        }
+        
     }
 
-    if (winCount > loseCount) {
-        console.log("You won!");
-    } else if (loseCount > winCount) {
-        console.log("You lose!")
-    } else {
-        console.log("It was a tie!");
+    switch (selection) {
+        case "rock":
+            playerChoiceDisplay.querySelector(".choice-img").src = "Assets/rock.png";
+            playerChoiceDisplay.querySelector(".choice-title").innerText = "ROCK";
+            break;
+        case "paper":
+            playerChoiceDisplay.querySelector(".choice-img").src = "Assets/paper.png";
+            playerChoiceDisplay.querySelector(".choice-title").innerText = "PAPER";
+            break;
+        case "scissors":
+            playerChoiceDisplay.querySelector(".choice-img").src = "Assets/scissors.png";
+            playerChoiceDisplay.querySelector(".choice-title").innerText = "SCISSORS";
+            break;
     }
-}
 
+    let computerSelection = getComputerChoice().toLowerCase();
+
+    switch (computerSelection) {
+
+        case "rock":
+            computerChoiceDisplay.querySelector(".choice-img").src = "Assets/rock.png";
+            computerChoiceDisplay.querySelector(".choice-title").innerText = "ROCK";
+            break;
+        case "paper":
+            computerChoiceDisplay.querySelector(".choice-img").src = "Assets/paper.png";
+            computerChoiceDisplay.querySelector(".choice-title").innerText = "PAPER";
+            break;
+        case "scissors":
+            computerChoiceDisplay.querySelector(".choice-img").src = "Assets/scissors.png";
+            computerChoiceDisplay.querySelector(".choice-title").innerText = "SCISSORS";
+            break;
+    
+        }
+
+        confirmButton.classList.add("hide");
+
+        playGame(selection, computerSelection); 
+        
+        playerScore.innerText = winCount;
+        computerScore.innerText = loseCount;
+
+        if (winCount == 5) {
+
+            mainText.innerText = "You win!";
+
+        } 
+
+        if (loseCount == 5) {
+
+            mainText.innerText = "You lose!";
+
+        }
+
+        if (winCount == 5 || loseCount == 5) {
+
+            choiceContainers.forEach(choice => {
+                
+                choice.classList.add("unavailable");
+
+            });
+
+            restartButton.classList.remove("hide");
+
+        }
+
+        restartButton.addEventListener("click", () => {
+
+            location.reload();
+
+        })
+
+})
